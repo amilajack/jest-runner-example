320b480db6c0d26bee9d33fc48d1c46e6b5fad2b
Amila Welihinda <amilaw@nvidia.com>
2018-06-12 11:40:48 -0700

    Moved test dir

--- index.spec.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv')
-
-
-describe('Foobar', () => {
-    it('should fooba', () => {
-        expect(1 + 1).toEqual(2)
-    })
-})
---
+++ tests/index.spec.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv')
+
+describe('Foobar', () => {
+  beforeAll(() => {
+    
+  })
+
+  it('should foobar', () => {
+    expect(1 + 1).toEqual(2)
+  })
+})
